8329a46bb6507f470b62f7087b9ad83ce9ccec31
pawankumargupta9142-glitch <pawankumargupta9142-glitch/stream-anomaly-engine>
2026-09-01 10:53:25 +0530

    Fix deployed API connection

--- backend/main.py
+++ backend/main.py
@@ -124,7 +124,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500"
-        
+         "https://stream-anomaly-engine-1.onrender.com"
     ],
 
     allow_credentials=True,
